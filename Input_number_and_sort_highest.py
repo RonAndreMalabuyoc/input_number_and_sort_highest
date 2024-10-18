49883e509f6d43a4eb8f0f1431250ed4c738f34d
RonAndreMalabuyoc <rondremalabuyoc@gmail.com>
2024-10-18 23:15:51 +0800

    sorting algorithm done

--- Input_number_and_sort_highest.py
+++ Input_number_and_sort_highest.py
@@ -3,3 +3,20 @@ num2 = int(input("Enter the second number here: "))
 num3 = int(input("Enter the third number here: "))
 num4 = int(input("Enter the forth number here: "))
 num5 = int(input("Enter the fifth number here: "))
+
+highest = num1
+
+if num2 > highest:
+    highest = num2
+
+if num3 > highest:
+    highest = num3
+
+if num4 > highest:
+    highest = num4
+
+if num5 > highest:
+    highest = num5
+
+
+print("The highest number is:", highest)
